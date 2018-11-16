cfee554fa6bfa3447a5e96081d92f1efb1146377
miguelarteaga <marteagavalle@gmail.com>
2018-11-16 18:51:24 +0100

    PrimerCommit

---
+++ PR3.1-PHP+MYSQL/ciudades.php
@@ -0,0 +1,52 @@
+<html>
+ <head>
+ 	<title>Exemple de lectura de dades a MySQL</title>
+ 	<style>
+ 		body{
+ 		}
+ 		table,td {
+ 			border: 1px solid black;
+ 			border-spacing: 0px;
+ 		}
+ 	</style>
+ </head>
+ 
+ <body>
+ 	
+ 
+ 	<?php
+ 		$codigo=$_GET["paises"];
+ 		$conn = mysqli_connect('localhost','miguel','miguel123');
+ 
+ 		mysqli_select_db($conn, 'world');
+ 
+ 	
+ 		$consulta = "SELECT country.Name COUNTRY, city.Name CITY FROM country, city WHERE country.Code=city.CountryCode AND country.Code='$codigo';";
+ 
+ 		
+ 		$resultat = mysqli_query($conn, $consulta);
+ 
+ 		
+ 	?>
+ 
+ 	<table>
+
+ 	<thead><td colspan="4" align="center" bgcolor="cyan">Llistat de ciutats</td></thead>
+ 	<?php
+ 
+ 		while( $registre = mysqli_fetch_assoc($resultat) )
+ 		{
+ 		
+ 			echo "\t<tr>\n";
+ 
+ 			
+ 			echo "\t\t<td>".$registre["CITY"]."</td>\n";
+ 			echo "\t\t<td>".$registre["COUNTRY"]."</td>\n";
+ 
+ 			
+ 			echo "\t</tr>\n";
+ 		}
+ 	?>
+ 	</table>	
+ </body>
+</html>
---
+++ PR3.1-PHP+MYSQL/paises.php
@@ -0,0 +1,46 @@
+<html>
+ <head>
+    <title>Exemple de lectura de dades a MySQL</title>
+    
+ </head>
+ 
+ <body>
+    <h1></h1>
+	
+ 
+    <?php
+        
+        $conn = mysqli_connect('localhost','miguel','miguel123');
+ 
+       
+        mysqli_select_db($conn, 'world');
+ 
+        
+        $consulta = "SELECT Name , Code FROM country;";
+ 
+        
+        $resultat = mysqli_query($conn, $consulta);
+ 
+        
+        if (!$resultat) {
+                $message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
+                $message .= 'Consulta realitzada: ' . $consulta;
+                die($message);
+        }
+
+        echo "<form action='ciudades.php' method='get' target='_blank'>";
+        echo "<select name='paises'>";
+        while( $registre = mysqli_fetch_assoc($resultat) ){
+            echo '<option value='.$registre["Code"].'>'.$registre["Name"].'</option>';
+        }
+        echo "<input type='submit' name='Submit'>";
+        echo "</select>";
+        echo"</form>";
+        
+        
+    ?>
+    <!-- (3.6) tanquem la taula -->
+    <!--select city.Name from city,country WHERE city.CountryCode=country.Code AND  city.CountryCode="AND";
+	-->  
+ </body>
+</html>
